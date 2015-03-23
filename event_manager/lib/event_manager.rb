2810a8fe256d839a179af7a3f3e2863ac7e00e38
Jacob Powers <powersjcb@gmail.com>
2015-03-23 00:08:20 -0700

    Finished event_manager email functionality

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,12 +1,52 @@
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+
+# helper methods
+def legislators_by_zipcode(zipcode)
+
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+end
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def save_thank_you_letters(id,form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+  File.open(filename,"w") do |file|
+    file.puts form_letter
+  end
+end
+
+# running the manager
 puts "EventManager Initialized"
 
+contents = CSV.open "./lib/event_attendees.csv", headers: true, header_converters: :symbol
 
-# lines = File.readlines "./event_attendees.csv"
+template_letter = File.read "lib/form_letter.html.erb"
+erb_template = ERB.new template_letter
 
-# lines.each_with_index do |line, index|
-#   next if index == 0
-#   columns = line.split(',')
-#   name = columns[2]
-#   puts name
-# end
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id,form_letter)
+end
+
+
+
+
+
 
